e9bd183b88b767a8ed35f5302af3e22c48717dd4
Yifei Dong <yifeidong@Yifeis-MacBook-Pro.local>
2018-03-18 18:04:21 -0400

    part 7 added

--- a3/fake.py
+++ a3/fake.py
@@ -2,8 +2,9 @@ import numpy as np
 import operator
 import math
 import random
-from build_sets import *
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+from sklearn import tree
+import graphviz 
 
 #=============== PART 1 ================================#
 
@@ -106,6 +107,8 @@ def Naive_Bayes_classifier(headline, word_list, training_set, training_label, m,
     
     prob_word_real = []
     prob_word_fake = []
+    prob_not_word_real = []
+    prob_not_word_fake = []    
     for i in word_list.keys():
         #P(word_i|real)
         P_word_i_real = (word_list[i][0]+m*p)/float(count_real + 1)
@@ -115,9 +118,13 @@ def Naive_Bayes_classifier(headline, word_list, training_set, training_label, m,
         if i in headline:
             prob_word_real.append(P_word_i_real)
             prob_word_fake.append(P_word_i_fake)
+            prob_not_word_real.append(1. - P_word_i_real)
+            prob_not_word_fake.append(1. - P_word_i_fake)            
         elif i not in headline:
             prob_word_real.append(1. - P_word_i_real)
             prob_word_fake.append(1. - P_word_i_fake)
+            prob_not_word_real.append(P_word_i_real)
+            prob_not_word_fake.append(P_word_i_fake)            
     
     #conditional independence is assumed by Naive Bayes
     #do multiplication to get P(words|real) and P(words|fake)
@@ -131,18 +138,25 @@ def Naive_Bayes_classifier(headline, word_list, training_set, training_label, m,
         multi_fake += math.log(p)
     multi_fake = math.exp(multi_fake)
     
+    #compute P(class)*P(words|class)
     prob_real_words = prob_real * multi_real
     prob_fake_words = prob_fake * multi_fake
     
+    #compute P(class)*(1 - P(words|class)) for part 3
+    prob_real_not_words = prob_real * (1. - multi_real)
+    prob_fake_not_words = prob_fake * (1. - multi_fake)   
     
     #probability that the given headline is fake, P(fake|words)
     prob = prob_fake_words/ (prob_fake_words + prob_real_words)
+    
+    #probability that the headline is fake when the word absence, P(fake|~words), for part 3
+    prob_absence = prob_fake_not_words/ (prob_fake_not_words + prob_real_not_words)    
     
     result = "real"
     if prob > 0.5:
         result = "fake"
     
-    return result, prob
+    return result, prob, prob_absence
 
 def test_part2():
     m = 1
@@ -151,11 +165,11 @@ def test_part2():
     count_train = 0
     n_train = len(train_real) + len(train_fake)
     for headline in train_real:
-        result, prob_fake = Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
+        result, prob_fake, prob_absence = Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
         if result == "real":
             count_train += 1
     for headline in train_fake:
-        result, prob_fake = Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
+        result, prob_fake, prob_absence= Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
         if result == "fake":
             count_train += 1
     performance_train = count_train / float(n_train) * 100
@@ -164,11 +178,11 @@ def test_part2():
     count_val = 0
     n_val = len(validate_real) + len(validate_fake)
     for headline in validate_real:
-        result, prob_fake = Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
+        result, prob_fake, prob_absence = Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
         if result == "real":
             count_val += 1
     for headline in validate_fake:
-        result, prob_fake = Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
+        result, prob_fake, prob_absence = Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
         if result == "fake":
             count_val += 1
     performance_val = count_val / float(n_val) * 100
@@ -177,11 +191,11 @@ def test_part2():
     count_test = 0
     n_test = len(test_real) + len(test_fake)
     for headline in test_real:
-        result, prob_fake = Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
+        result, prob_fake, prob_absence = Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
         if result == "real":
             count_test += 1
     for headline in test_fake:
-        result, prob_fake = Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
+        result, prob_fake, prob_absence = Naive_Bayes_classifier(headline, word_list, train_real, train_fake, m, p)
         if result == "fake":
             count_test += 1
     performance_test = count_test / float(n_test) * 100
@@ -201,32 +215,18 @@ def part3a(word_list):
     prob_fake_word = np.array([])
     prob_fake_not_word = np.array([])
     
-    #compute P(fake|word) for all words
+    #compute P(fake|word) and P(fake|~word) for all words
     for word in word_list.keys():
         words = np.append(words, word)
         word = list(word)
-        result, prob_fake = Naive_Bayes_classifier(word, word_list, train_real, train_fake, 1, 0.1)
+        result, prob_fake, prob_absence = Naive_Bayes_classifier(word, word_list, train_real, train_fake, 1, 0.1)
         prob_fake_word = np.append(prob_fake_word, prob_fake)
+        prob_fake_not_word = np.append(prob_fake_not_word, prob_absence)
     
-    #compute P(real|word) for all words
+    #compute P(real|word) and P(real|~word) for all words
     for i in range(len(prob_fake_word)):
-        prob_real_word = np.append(prob_real_word, 1 - prob_fake_word[i])
-        
-    #compute P(fake|~word) for all words
-    for word in words:
-        #P(fake|~word) = P(fake|all words) - P(fake|word)
-        P_fake_words = np.sum(prob_fake_word)
-        P_fake_word = prob_fake_word[np.where(words == word)]
-        prob_fake_not_word = np.append(prob_fake_not_word, (P_fake_words - P_fake_word)/P_fake_words)
-    
-    #compute P(real|~word) for all words
-    for word in words:
-        #P(real|~word) = P(real|all words) - P(real|word)
-        P_real_words = np.sum(prob_real_word)
-        P_real_word = prob_real_word[np.where(words == word)]
-        prob_real_not_word = np.append(prob_real_not_word, (P_real_words - P_real_word)/P_real_words)
-    
-    # print(words.shape, P_fake_given_word.shape, P_fake_given_not_word.shape, P_real_given_word.shape, P_real_given_not_word.shape)
+        prob_real_word = np.append(prob_real_word, 1. - prob_fake_word[i])
+        prob_real_not_word = np.append(prob_real_not_word, 1. - prob_fake_not_word[i])
     
     real_presence = dict(zip(words, prob_real_word))
     real_absence = dict(zip(words, prob_real_not_word))
@@ -249,7 +249,7 @@ def part3a(word_list):
 
 def top10(rank):
     """
-    print the top ten word in an array
+    print the last ten word in ascending array.
     """
     i = -1
     while i > -11:    
@@ -266,3 +266,89 @@ def part3b():
     
     
 #=============== PART 4 ================================#
+
+
+
+
+
+
+#=============== PART 7 ================================#
+
+def part7():
+    #part7a
+    #-----Train the decision tree classifier, find the best parameters-----------
+    training = np.append(train_real, train_fake)
+    training_label = [1] * len(train_real) + [0] * len(train_fake)
+    
+    validation = np.append(validate_real, validate_fake)
+    validation_label = [1] * len(validate_real) + [0] * len(validate_fake)
+    
+    test = np.append(test_real, test_fake)
+    test_label = [1] * len(test_real) + [0] * len(test_fake)
+    
+    #assign each word a unique number and save the number of total unique words as num_words
+    word_index = {}
+    num_words = 0
+    for headline in training:
+        for word in headline:
+            if word not in word_index: 
+                word_index[word] = num_words
+                num_words += 1
+    for headline in validation:
+        for word in headline:
+            if word not in word_index: 
+                word_index[word] = num_words
+                num_words += 1
+    for headline in test:
+        for word in headline:
+            if word not in word_index: 
+                word_index[word] = num_words
+                num_words += 1
+    """
+    make training set, validation set and testing set into 2-D numpy arrays which shows occurrence of words in each headline.
+    For a single headline, the length of the array is the number of unique words. At each index of the array, it is 1 if the word is
+    in the headline, it is 0 otherwise.
+    """
+    training_set = np.zeros((0, num_words))
+    validation_set = np.zeros((0, num_words)) 
+    test_set = np.zeros((0, num_words))
+    
+    for headline in training:
+        i = np.zeros(num_words)
+        for word in headline:
+            i[word_index[word]] = 1.
+        i = np.reshape(i, [1, num_words])
+        training_set = np.vstack((training_set, i))
+    for headline in validation:
+        i = np.zeros(num_words)
+        for word in headline:
+            i[word_index[word]] = 1.
+        i = np.reshape(i, [1, num_words])
+        validation_set = np.vstack((validation_set, i))
+    for headline in test:
+        i = np.zeros(num_words)
+        for word in headline:
+            i[word_index[word]] = 1.
+        i = np.reshape(i, [1, num_words])
+        test_set = np.vstack((test_set, i))  
+    
+    max_depth_list = [3, 10, 20, 50, 100, 150, 200, 300, 500, 700]
+    
+    for depth in max_depth_list:
+        clf = tree.DecisionTreeClassifier(max_depth=depth)
+        clf = clf.fit(training_set, training_label)
+        print("Max depth is: " + str(depth))
+        print("Training: " + str(100*clf.score(training_set, training_label)) + " Validation: " + str(100*clf.score(validation_set, validation_label)))
+        
+    #Highest accuracy is achived at max_depth 500
+    clf = tree.DecisionTreeClassifier(max_depth=300)
+    clf = clf.fit(training_set, training_label)    
+
+    #part7b
+    word = []
+    for i in word_index.keys():
+        word.append(i)  
+    
+    dot_data = tree.export_graphviz(clf, out_file=None, max_depth=2, filled=True, rounded=True, class_names=['fake', 'real'], feature_names=word)
+    graph = graphviz.Source(dot_data)
+    graph.render(view=True)
